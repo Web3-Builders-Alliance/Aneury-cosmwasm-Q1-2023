9fa90e0a4791c2eb93467cadbd9665b23b6e7174
Aneury Perez <aneury1@gmail.com>
2023-01-29 13:51:56 -0400

    Generating const values with const help to make our code more clear and consise but we need to put type into the declaration

--- Prerequisites/RustCrashCourse/src/vars.rs
+++ Prerequisites/RustCrashCourse/src/vars.rs
@@ -6,8 +6,8 @@
 pub fn run(){
    let name ="Aneury";
    let mut age = 34;
-    
+   const ID: i32 = 123;
    age = 35;
 
-   println!("My name is : {} and I am {}", name, age);
+   println!("My name is : {} and I am {} and id selected {}", name, age, ID);
 }

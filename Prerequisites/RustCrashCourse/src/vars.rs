3ed438993f7f1da277eda3ab055d8fa71d7aae25
Aneury Perez <aneury1@gmail.com>
2023-01-29 13:49:10 -0400

    Variaables are immutable by default

---
+++ Prerequisites/RustCrashCourse/src/vars.rs
@@ -0,0 +1,13 @@
+/// Variables?
+/// Variables are immutables by default
+/// Rust 
+/// 
+
+pub fn run(){
+   let name ="Aneury";
+   let age = 34;
+    
+   ///age = 35;
+
+   println!("My name is : {} and I am {}", name, age);
+}

0d431fe2ca60fd48bdbbf9c972fa7f8c575112c9
Aneury Perez <aneury1@gmail.com>
2023-01-28 22:03:10 -0400

    Named argument

--- Prerequisites/RustCrashCourse/src/print.rs
+++ Prerequisites/RustCrashCourse/src/print.rs
@@ -1,6 +1,12 @@
 
 
 pub fn run(){
+    /// printing without argument
     println!("Hello from print.rs ");
+
+    /// Basic formarter
     println!("print a single argument Value = {}", 1);
+
+    /// Named Argument!!
+    println!("print a single argument Value = {id} {number}", number=1, id=123);
 }

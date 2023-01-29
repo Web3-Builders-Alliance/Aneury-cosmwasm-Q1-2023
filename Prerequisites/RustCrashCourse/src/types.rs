4c216fff452170d5c4cc8adf8e74f1a94aa7fae5
Aneury Perez <aneury1@gmail.com>
2023-01-29 14:09:55 -0400

    Types and max number

---
+++ Prerequisites/RustCrashCourse/src/types.rs
@@ -0,0 +1,10 @@
+
+
+pub fn run(){
+    let x = 1;   // i32
+    let y = 2.5; // f64
+    let z: i64 = 435343434434344; // i64
+    
+    println!("Max i32 {}", std::i32::MAX );
+    println!("Max i32 {}", std::i64::MAX );
+}

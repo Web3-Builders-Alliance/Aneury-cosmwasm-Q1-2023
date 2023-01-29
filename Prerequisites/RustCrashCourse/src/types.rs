561ecb1089ed6eaf8d806ca400ecd05b4a930ed3
Aneury Perez <aneury1@gmail.com>
2023-01-29 14:14:01 -0400

    Data types and encoding

--- Prerequisites/RustCrashCourse/src/types.rs
+++ Prerequisites/RustCrashCourse/src/types.rs
@@ -5,6 +5,17 @@ pub fn run(){
     let y = 2.5; // f64
     let z: i64 = 435343434434344; // i64
     
+    let is_active: bool = true;
+
+    let is_greater : bool = 1>2;
+
+    let a1 = 'a';
+
+    let face = '\u{1F600}';
+
     println!("Max i32 {}", std::i32::MAX );
     println!("Max i32 {}", std::i64::MAX );
+
+
+    println!("{:?}",(x,y,z,is_active,is_greater,face));
 }
